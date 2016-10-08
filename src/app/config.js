1505fdf4313f4a852b1f47f7f9a55bfe59f43820
Jeff Friesen <momoblackblack@gmail.com>
2016-10-08 13:04:05 -0600

    adding firebase keys to starter project

--- src/app/config.js
+++ src/app/config.js
@@ -2,12 +2,10 @@ module.exports = {
 
   // Change this to your firebase configuration! (Add Firebase to your web app)
   FIREBASE_CONFIG: {
-
-    apiKey: "AIzaSyAAzWIkcMqyHueh1yQt84HdpMFfzL1OZYc",
-    authDomain: "react-redux-firebase-d6283.firebaseapp.com",
-    databaseURL: "https://react-redux-firebase-d6283.firebaseio.com",
-    storageBucket: "react-redux-firebase-d6283.appspot.com",
-    messagingSenderId: "425723634684"
-
+    apiKey: "AIzaSyBM2F7rjig2iSkkXnsqPn9Yfff9",
+    authDomain: "react-mobx-firebase-starter.firebaseapp.com",
+    databaseURL: "https://react-mobx-firebase-starter.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "267749915553"
   }
-};
+}
